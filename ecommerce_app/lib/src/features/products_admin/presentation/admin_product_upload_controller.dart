8e722d99123a35d35a387f475ddd0f75a629d55c
Mlungisi93 <215229827@edu.vut.ac.za>
2025-02-14 07:55:16 +0200

    Modlule 4 firestore and firebase until lesson 08- completing image upload

---
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
@@ -0,0 +1,42 @@
+import 'package:ecommerce_app/src/features/products/data/products_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:ecommerce_app/src/utils/notifier_mounted.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'admin_product_upload_controller.g.dart';
+
+@riverpod
+class AdminProductUploadController extends _$AdminProductUploadController
+    with NotifierMounted {
+  @override
+  FutureOr<void> build() {
+    ref.onDispose(setUnmounted);
+    // no-op
+  }
+
+  Future<void> upload(Product product) async {
+    try {
+      state = const AsyncLoading();
+      final downloadUrl = await ref
+          .read(imageUploadRepositoryProvider)
+          .uploadProductImageFromAsset(product.imageUrl, product.id);
+      // save downloadUrl to Firestore
+      await ref
+          .read(productsRepositoryProvider)
+          .createProduct(product.id, downloadUrl);
+      //we should not set the state if we are navigating on different page on success but we should do it on error instead
+      // if (mounted) {
+      //   state = const AsyncData(null);
+      // }
+      //on success, go to the edit product page
+      ref.read(goRouterProvider).goNamed(AppRoute.adminEditProduct.name,
+          pathParameters: {'id': product.id});
+    } catch (e, st) {
+      if (mounted) {
+        state = AsyncError(e, st);
+      }
+    }
+  }
+}
